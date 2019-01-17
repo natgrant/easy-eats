61a59da42c40acca525b7b6a5344d268815bcb07
Alex Huang <kuan8@hotmail.com>
2019-01-17 17:44:28 +1300

    set up routes and data.json

---
+++ routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router();
+const data = require('./data.json')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+module.exports = router
+
+router.get('/', (req, res) => {
+
+  res.render('index',data)
+})
+
+
+
+
+router.get('/?/:id', (req, res) => {
+  var result = data.?.find(function (item) {
+    return item.id == req.params.id
+  })
+  res.render('index/view', result)
+})
