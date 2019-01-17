1cc02873c3e71a9c133fa2d3c45b24107288bdb1
Nat Grant <email.natgrant@gmail.com>
2019-01-18 12:15:17 +1300

    updating main hbs

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.engine('hbs', hbs({
   server.use(express.static('public'))
   server.use(express.urlencoded({extended: false}))
   server.use('/',routes)
-  server.set('views', __dirname + '/views');
+  server.set('views', __dirname + '/views/partials/navbar');
 
 
 
