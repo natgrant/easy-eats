f2e57d404a948cb8defa5b670616c839bdc38560
ranjit03 <ranjitendurthi03@gmail.com>
2019-01-19 16:54:35 +1300

    all link on home page working

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
 const express = require ('express')
-//const hbs = require ('express-handlebars')
+const hbs = require ('express-handlebars')
 const routes = require('./routes')
 const app = express ()
 
-// Middleware
-// app.engine('hbs', hbs({
-//   extname: 'hbs',
-//   defaultLayout: 'main',
+//Middleware
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
     
-//   }))
+  }))
   app.set('view engine', 'hbs')
   app.use(express.static('public'))
   app.use(express.urlencoded({extended: false}))
   app.use('/',routes)
   
 
-  var exphbs = require('express-handlebars');
-  var hbsHelpers = exphbs.create({
-      helpers: require("./helpers/handlebars.js").helpers,
-      defaultLayout: 'main',
-      extname: '.hbs'
-  });
-  
-  app.engine('.hbs', hbsHelpers.engine);
-  app.set('view engine', '.hbs');
-
-  hbs.registerHelper('for', function(from, to, incr, block) {
-    var accum = '';
-    for(var i = from; i < to; i += incr)
-        accum += block.fn(i);
-    return accum;
-});
 
   module.exports = app
